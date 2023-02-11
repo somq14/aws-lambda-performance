7aed3af4dbfd58ac517f666ab6dfc13b5c72b4f0
somq14 <som.q14@gmail.com>
2023-02-11 16:40:40 +0900

    ✨ AWS CDK

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export const add = (a: number, b: number): number => {
-  return a + b;
-};
+import * as cdk from "aws-cdk-lib";
+
+import { AppStack } from "./app-stack";
+
+const app = new cdk.App();
+new AppStack(app, "aws-lambda-performance", {});
